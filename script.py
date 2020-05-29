483da6cc5b8075f8c864999c2ced40f95f11c009
zinhart <aromzinhartdegraca@gmail.com>
2020-05-28 20:45:12 -0700

    Log in chrome working

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import platform
 import winreg
+import sys
+from time import sleep
 from selenium import webdriver
 supported_os = ['windows', 'ubuntu']
 supported_browsers = { 'windows' : {"chrome.exe":"", "iexplore.exe":"", "microsoftedge.exe":""} ,
@@ -30,7 +32,6 @@ def get_all_browsers(operating_system):
         for n in range(acess_key_count):
             try:
                 browser = str.lower(winreg.EnumKey(access_key, n))
-                print(browser)
                 if browser in windows_browsers.keys():
                     app_path = apps+str("\\")+browser
                     handle = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, app_path)
@@ -45,24 +46,22 @@ def get_all_browsers(operating_system):
 
 def get_browser_path(os_name, browser_name):
     all_browsers = get_all_browsers(os_name)
-    print(all_browsers)
     if browser_name in all_browsers.keys():
             return str(all_browsers[browser_name] + "\\" + browser_name)
     else:
         print("error unsupported browser")
         return None
-def get_web_driver(os_name, browser_name, browser_path):
+def get_web_driver(os_name, browser_name):
     if os_name == 'windows':
         if browser_name == 'chrome.exe':
-            supported_webdrivers[os_name][browser_name] = webdriver.Chrome(browser_path)
+            supported_webdrivers[os_name][browser_name] = webdriver.Chrome()
         elif browser_name == 'iexplore.exe':
-            supported_webdrivers[os_name][browser_name] = webdriver.Ie(browser_path)
+            supported_webdrivers[os_name][browser_name] = webdriver.Ie()
         elif browser_name == 'microsoftedge.exe':
-            supported_webdrivers[os_name][browser_name] = webdriver.Edge(browser_path)
+            supported_webdrivers[os_name][browser_name] = webdriver.Edge()
         return supported_webdrivers[os_name][browser_name]
 
-def main(browser_name, url):
-    print(supported_browsers)
+def main(browser_name, url, usr, pw):
     os_name = get_os()
     if os_name not in supported_os:
             print("unsupported os")
@@ -71,10 +70,30 @@ def main(browser_name, url):
     if browser_path == None:
         print("browser: ",browser," not found")
         return None
-    driver = get_web_driver(os_name, browser_name, browser_path)
+    driver = get_web_driver(os_name, browser_name)
     driver.get(url)
+    username = driver.find_element_by_id("username")
+    password = driver.find_element_by_id("password")
+    username.send_keys(usr)
+    sleep(0.5)
+    password.send_keys(pw)
+    sleep(0.5)
+
+#    sign_in_button = driver.find_element_by_class_name('signin')
+    sign_in_button = driver.find_element_by_xpath('//*[@type="submit"]')
+    sign_in_button.click()
 
 
 
 if __name__ == '__main__':
-    main("chrome.exe", 'https://www.linkedin.com')
+    '''
+    if len(sys.argv) != 5:
+        print("error")
+        exit()
+    '''
+    browser_name = sys.argv[1]
+    url = sys.argv[2]
+    usr = sys.argv[3]
+    pw  = sys.argv[4]
+    print(browser_name, url, usr, pw)
+    main("chrome.exe", 'https://www.linkedin.com/login', usr, pw)
